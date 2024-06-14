5e9503549a944eb971ada4039a0aabd4f902a75a
FredzyW <fredrik@wastring.com>
2024-06-14 17:23:02 +0200

    Changed colors of dwm status

--- config.h
+++ config.h
@@ -15,6 +15,8 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=12" };
 static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=12";
 static const char black[]       = "#1E1D2D";
+static const char base[] 		= "#24273a";
+static const char text[] 		= "#cad3f5";
 static const char gray2[]       = "#282737"; // unfocused window border
 static const char gray3[]       = "#585767";
 static const char gray4[]       = "#282737";
@@ -34,8 +36,8 @@ static const char white[] = "#f8f8f2";
 // static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { gray3, black, gray2 },
-	[SchemeSel]  = { gray4, peach,  peach  },
+	[SchemeNorm] = { text, base, base },
+	[SchemeSel]  = { text, peach,  peach  },
 };
 
 /* tagging */
