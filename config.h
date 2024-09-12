696050185fa48575e53157e83d46ae0613733446
FredzyW <fredrik@wastring.com>
2024-09-12 16:58:02 +0200

    Forgot semicolon

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", base, "-nf", text, "-sb", peach, "-sf", base, NULL };
-static const char *passcmd[] = { "passmenu", "-p", "pass", "-fn", dmenufont, "-nb", base, "-nf", text, "-sb", peach, "-sf", base}
+static const char *passcmd[] = { "passmenu", "-p", "pass", "-fn", dmenufont, "-nb", base, "-nf", text, "-sb", peach, "-sf", base};
 static const char *termcmd[]  = { "alacritty", "--title", "Alacritty" };
 static const char *browsercmd[]  = { "firefox", NULL };
 static const char *aerccmd[] = { "kitty", "-e", "aerc" };
