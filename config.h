e298bf0dac111f928ef0fbd4a7ebc21db8782df5
fwastring <fredrik@wastring.com>
2025-02-03 10:01:03 +0100

    Added dmenucmd

--- config.h
+++ config.h
@@ -82,6 +82,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "rofi", "-show", "run"};
 static const char *launchcmd[] = { "rofi", "-show", "run"};
 static const char *termcmd[]  = { "kitty", "--title", "Alacritty" };
 static const char *browsercmd[]  = { "firefox", NULL };
