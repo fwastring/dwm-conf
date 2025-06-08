b50e98d6921e7ade6658576ece17a04e19f4f564
fwastring <fredrik@wastring.com>
2025-06-08 10:57:50 +0200

    build: 10:57:50

--- config.h
+++ config.h
@@ -76,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-bw", "3"};
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-c", "-bw", "3"};
 static const char *termcmd[]  = { "st" };
 static const char *syscmd[]  = { "sh", "/home/fw/scripts/dmenu_sys" };
 static const char *browsercmd[]  = { "qutebrowser", NULL };
